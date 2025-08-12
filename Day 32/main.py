883660981020c97524d48b8dd1065143daa4860b
aokenu <austineokenu@gmail.com>
2025-08-12 10:42:16 +0100

    Updated files for Day 32

--- Day 32/main.py
+++ Day 32/main.py
@@ -1,22 +1,44 @@
 import smtplib
 import ssl
 from email.mime.text import MIMEText
+import random
+import datetime as dt
+
+DAY = 1
+# Open the txt file containing the quotes
+with open("quotes.txt", "r", encoding="utf-8") as f:
+    my_text = f.readlines()
+    stripped_text = [line.strip() for line in my_text]
+    message = random.choice(stripped_text)
 
 
-with open("message.txt", "r", encoding="utf-8") as f:
-    message = f.read()
 
+# Email setup
 smtp_server = "smtp.gmail.com"
 port = 587
 my_email = "datadotng@gmail.com"
 password = "bjlvycgbqgtzkoek"
 msg = MIMEText(message, "plain", "utf-8")
-msg["Subject"] = "Hallo aus meiner Python-App"
+msg["Subject"] = "Daily Motivational Quote"
+msg["From"] = my_email
+msg["To"] = "chibuikeokenu@gmail.com"
+
+# Getting the day of the week
+day_of_the_week = dt.datetime.today().weekday()
+
+# conditional statement to send email
+if day_of_the_week == DAY:
+    with smtplib.SMTP(smtp_server, port) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(my_email, ["chibuikeokenu@gmail.com"], msg.as_string())
+
+        print("Email sent successfully")
+else:
+    print("Today is not the day")
 
 
 
-connection = smtplib.SMTP(smtp_server, port)
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="chibuikeokenu@gmail.com", msg=msg.as_string())
-connection.close()
+
+
+
