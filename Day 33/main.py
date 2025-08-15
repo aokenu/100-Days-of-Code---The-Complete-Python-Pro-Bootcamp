b62ccf411e09d71c082f39c8841b020eef552d5d
aokenu <austineokenu@gmail.com>
2025-08-16 00:40:13 +0100

    Added Day 34 files

--- Day 33/main.py
+++ Day 33/main.py
@@ -1,14 +1,33 @@
 import requests
+from datetime import datetime
 
-URL = "http://api.open-notify.org/iss-now.json"
+# URL = "http://api.open-notify.org/iss-now.json"
+#
+# response = requests.get(url=URL)
+# response.raise_for_status()
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+# print(iss_position)
 
-response = requests.get(url=URL)
+
+URL = "https://api.sunrise-sunset.org/json"
+
+MY_LAT = 6.451140
+MY_LONG = 3.388400
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
+response = requests.get(URL, parameters)
 response.raise_for_status()
-data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+location = response.json()
+sunrise = location["results"]["sunrise"]
+sunset = location["results"]["sunset"]
 
-iss_position = (longitude, latitude)
-print(iss_position)
-
-
+print(sunrise)
---
+++ Day 34/main.py
@@ -0,0 +1,47 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+import requests
+import json
+from tkinter import *
+
+window = Tk()
+
+
+URL = "https://opentdb.com/api.php"
+parameter = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+quiz_data = []
+
+def get_data():
+    global quiz_data
+    # Pass parameters correctly
+    response = requests.get(URL, parameter)
+
+    # Convert to JSON
+    data = response.json()
+    quiz_data = data["results"]
+
+
+get_data()
+
+
+question_bank = []
+for question in quiz_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
