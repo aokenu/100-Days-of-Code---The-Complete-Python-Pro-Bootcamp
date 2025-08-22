8120818d1515a449fa063ab47c718f0defd086cb
aokenu <austineokenu@gmail.com>
2025-08-22 01:09:27 +0100

    Added files for Day 35

--- Day 34/main.py
+++ Day 34/main.py
@@ -3,14 +3,11 @@ from data import question_data
 from quiz_brain import QuizBrain
 from ui import QuizInterface
 import requests
-<<<<<<< HEAD
 import json
 from ui import QuizInterface
 
-=======
 
 
->>>>>>> 6f0a0bcf2cf07156481035047b0a9452720e8448
 
 
 URL = "https://opentdb.com/api.php"
@@ -43,10 +40,10 @@ for question in quiz_data:
 
 
 quiz = QuizBrain(question_bank)
-quiz_ui = QuizInterface()
+quiz_ui = QuizInterface(quiz)
 
-while quiz.still_has_questions():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- Day 34/ui.py
+++ Day 34/ui.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR )
@@ -21,19 +24,52 @@ class QuizInterface:
             150,
             125,
             text="Some Question Text",
+            width= 280,
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
         )
         self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=50)
 
         # Create the two buttons
-        self.left_button = Button(image=self.left_image, highlightthickness=0)
+        self.left_button = Button(image=self.left_image, highlightthickness=0, command=self.true_pressed)
         self.left_button.grid(row=3, column=0, padx=20, pady=20)
-        self.right_button = Button(image=self.right_image, highlightthickness=0)
+        self.right_button = Button(image=self.right_image, highlightthickness=0, command=self.false_pressed)
         self.right_button.grid(row=3, column=1, padx=20, pady=20)
 
-
-
-
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.right_button.config(state="disabled")
+            self.left_button.config(state="disabled")
+
+
+    def true_pressed(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
+    def score_board(self):
+        global score
+        if self.give_feedback:
+            score += 1
+            return score
