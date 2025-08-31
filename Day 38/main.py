103f20ee789295f6de2fbfceb0e9c6cdf5a35a79
aokenu <34852856+aokenu@users.noreply.github.com>
2025-09-01 00:01:54 +0100

    Updated files for Day 39

--- Day 38/main.py
+++ Day 38/main.py
@@ -1,14 +1,16 @@
 import requests
 import json
+from datetime import datetime
+import os
 
 
 
-API_ID = "6bfe0f71"
-API_KEY = "5a05160438dbf870ca4aebd049bbb874"
+API_ID = os.environ["API_ID"]
+API_KEY = os.environ["API_KEY"]
 
 URL = "https://trackapi.nutritionix.com"
 
-QUERY = "I ate 2 teaspoons of milk today"
+QUERY = input("Tell me what you did today:")
 
 parameters = {
     "query": f"{QUERY}"
@@ -19,8 +21,47 @@ headers = {
     "x-app-key": API_KEY
 }
 
-ENDPOINT = f"{URL}/v2/natural/nutrients"
+ENDPOINT = f"{URL}/v2/natural/exercise"
 
 response = requests.post(ENDPOINT, parameters, headers=headers)
 data = response.json()
-print(json.dumps(data, indent=4))
+# print(json.dumps(data, indent=4))
+
+exercise = data["exercises"][0]["name"]
+# print(exercise)
+duration = data["exercises"][0]["duration_min"]
+# print(duration)
+calories = data["exercises"][0]["nf_calories"]
+# print(calories)
+
+date = datetime.now().strftime("%d/%m/%Y")
+
+datetime.now()
+# print(date)
+
+time = datetime.now().strftime("%X")
+# print(time)
+
+url = os.environ["SHEETY_URL"]
+body = {
+    "workout": {
+        "date": date,
+        "time": time,
+        "exercise": exercise,
+        "duration": duration,
+        "calories": calories
+    }
+}
+
+TOKEN = os.environ["SHEETY_TOKEN"]
+
+header = {
+    "Authorization": f"Bearer {TOKEN}"
+}
+
+update_workout = requests.post(url=url, json=body, headers=header)
+print(update_workout.text)
+
+
+# response = requests.get(url)
+# print(response.json())
