ff4794a0f45837e9c9b4252b951cab207dc5732f
aokenu <34852856+aokenu@users.noreply.github.com>
2025-10-15 14:11:47 +0100

    Updated the python file for Day 45

--- Day 45/bs4-start/main.py
+++ Day 45/bs4-start/main.py
@@ -1,53 +1,65 @@
 from bs4 import BeautifulSoup
+import requests
 # import lxml
 
 
 
-with open("Day 45/bs4-start/website.html") as file:
-    contents = file.read()
+# with open("Day 45/bs4-start/website.html") as file:
+#     contents = file.read()
 
 
-soup = BeautifulSoup(contents, "html.parser")
+# soup = BeautifulSoup(contents, "html.parser")
 
-title = soup.title
-# print(title)
+# title = soup.title
+# # print(title)
 
-# print(soup.prettify())
+# # print(soup.prettify())
 
-# print(soup.a)
+# # print(soup.a)
 
-# To find all the tags where the tag name is "a"
-all_anchor_tags = soup.find_all(name="a")
-print(all_anchor_tags)
+# # To find all the tags where the tag name is "a"
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
 
-# To get all the text within the anchor tags
-for tag in all_anchor_tags:
-    print(tag.getText())
+# # To get all the text within the anchor tags
+# for tag in all_anchor_tags:
+#     print(tag.getText())
 
-# To get the URLs of the href
-for tag in all_anchor_tags:
-    print(tag.get("href"))
+# # To get the URLs of the href
+# for tag in all_anchor_tags:
+#     print(tag.get("href"))
 
-# Finding h1 elements with id = "name"
-heading = soup.find_all(name="h1", id="name")
-print(heading)
-
-
-# Finding h3 elements with class = "class"
-section_heading = soup.find_all(name="h3", class_="heading")
-print(section_heading)
+# # Finding h1 elements with id = "name"
+# heading = soup.find_all(name="h1", id="name")
+# print(heading)
 
 
-# To locate the value of "a" tag inside a "p" tag
-company_url = soup.select_one(selector="p a")
-print(company_url.text)
+# # Finding h3 elements with class = "class"
+# section_heading = soup.find_all(name="h3", class_="heading")
+# print(section_heading)
 
 
-# To get the value of an id selector
-name = soup.select_one(selector="#name")
-print(name)
+# # To locate the value of "a" tag inside a "p" tag
+# company_url = soup.select_one(selector="p a")
+# print(company_url.text)
 
 
-# To locate an element by class
-headings = soup.select(".heading")
-print(headings)
+# # To get the value of an id selector
+# name = soup.select_one(selector="#name")
+# print(name)
+
+
+# # To locate an element by class
+# headings = soup.select(".heading")
+# print(headings)
+
+
+# the url of the website to scrap from
+url = "https://news.ycombinator.com/"
+response = requests.get(url) # calling the website url
+data = response.text
+
+soup = BeautifulSoup(data, 'html.parser') # creating a soup object
+
+article_title = soup.find(name="span", class_="titleline")
+print(article_title.text)
