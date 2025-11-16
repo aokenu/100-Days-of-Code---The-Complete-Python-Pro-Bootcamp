9dc3d181f2924044db6141d6a16fed0edac93efd
aokenu <34852856+aokenu@users.noreply.github.com>
2025-11-16 12:45:44 +0100

    Updated file for Day 48

--- Day 48/main.py
+++ Day 48/main.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
+# URL = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1&language=en_US&currency=USD"
 
+URL = "https://www.python.org/"
 
 # Keep Chrome browser open after program finishes
 chrome_options = webdriver.ChromeOptions()
@@ -9,11 +12,19 @@ chrome_options.add_experimental_option("detach", True)
 
 # creating an instance of the webdriver
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.amazon.com")
+driver.get(URL)
+
+# finding elements in the html content by class
+# price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+# price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+
+# print(f"The price is {price_dollar.text}.{price_cents.text}")
 
 
-# close the browser
-driver.close()
+# finding elements by name
+get_input = driver.find_element(By.NAME, value="q")
+print(get_input.text)
+
 
 # close the entire program
 driver.quit()
