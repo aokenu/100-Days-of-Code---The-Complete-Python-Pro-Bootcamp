615c8bc5a95ea6ac431a9059b163b90069623d05
aokenu <34852856+aokenu@users.noreply.github.com>
2025-11-16 17:23:49 +0100

    Update file for Day 48

--- Day 48/main.py
+++ Day 48/main.py
@@ -14,36 +14,24 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
 
 
+event_dict = {} # create an empty list
+events = number_of_event = driver.find_elements(By.XPATH, value='//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li') # get the total number of all the events
 
-event_list = []
-events = number_of_event = driver.find_elements(By.XPATH, value='//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li')
-event_count = len(events)
-# for event in events:
-#     event_list.append(event.text)
-    #print(event_list)
-range_count = [number for number in range(event_count)]
+# loop over the total events to extact each event and date
+for index, event in enumerate(events, start=0):
+    time_el = event.find_element(By.TAG_NAME, 'time').text # looks inside that <li> for the first <time> child element and returns another WebElement.
+    name_el = event.find_element(By.TAG_NAME, 'a').text # looks inside that <li> for the first <time> child element and returns another WebElement.
 
-all_events = [range_count]
-
-print(range_count)
-#print(event_count)
-
-
+    # add each of the events,date to the event_dict
+    event_dict[index] = {
+            "time": time_el,
+            "name": name_el
+            }   
 
 
-# finding the event date by xpath
-# event_date = driver.find_element(By.XPATH, value='//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li[1]/time')
+print(event_dict)
 
 
-# # finding the event name by xpath
-# event_name = driver.find_element(By.XPATH, value='//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li[1]/a')
 
-
-# # print(f"'time': {event_date.text}, 'name': {event_name.text}")
-
-# number_of_event = driver.find_elements(By.XPATH, value='//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li[1]/time')
-
-# # for event in number_of_event:
-# #     print(event.text)
 
 driver.quit()
