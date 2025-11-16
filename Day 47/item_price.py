396e162b15d73cce586f0299cf19f069e03e6fb2
aokenu <34852856+aokenu@users.noreply.github.com>
2025-11-16 01:20:33 +0100

    Updated files for Day 47 project

--- Day 47/item_price.py
+++ Day 47/item_price.py
@@ -7,19 +7,33 @@ class PriceTag:
     
     def __init__(self):
 
-        self.URL = "https://appbrewery.github.io/instant_pot/"
+        self.URL = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1&language=en_US&currency=USD"
+        self.header = {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
+                "Accept-Language": "en-US,en;q=0.9,it;q=0.8,de;q=0.7",  
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36", 
+            }        
 
         # calling the url with a get request
-        self.response = requests.get(self.URL)
+        self.response = requests.get(url=self.URL, headers=self.header)
         self.data = self.response.text
+
 
     def get_price(self):
         # create an instance of beatifulsoup
         self.soup = BeautifulSoup(self.data, 'html.parser')
 
         # extracting the content of the web page using the created instance of Beautifulsoup
-        web_page = self.soup.find(name="span", class_="a-price-whole")
-        self.get_price = float(web_page.text)
+        web_page = self.soup.find(name="span", class_="aok-offscreen")
+        web_text = web_page.text
+        get_price = float(web_text.split(" ")[1][1:])
+
 
         # return the output of the scrapped value
-        return self.get_price
+        return get_price
+   
+
+
+# price = PriceTag()
+# check_price = price.get_price()
+# print(check_price)
--- Day 47/notifier.py
+++ Day 47/notifier.py
@@ -12,13 +12,14 @@ class SendMail(PriceTag):
         self.auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
         self.client = Client(self.account_sid, self.auth_token)
         self.price = get_price
+        self.item_url = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1&language=en_US&currency=USD"
 
 
     def push_notification(self): # method to send sms notiticatiob
         if self.price < 100.00:
             # Create a Twilio client
             message = self.client.messages.create(
-                body=f"Instant Pot Duo Plus 9-in-1 Electric Pressure Cooker, Slow Cooker, Rice Cooker, Steamer, Sauté, Yogurt Maker, Warmer & Sterilizer, Includes App With Over 800 Recipes, Stainless Steel, 3 Quart is now {self.price} \n https://appbrewery.github.io/instant_pot/",
+                body=f"Instant Pot Duo Plus 9-in-1 Electric Pressure Cooker, Slow Cooker, Rice Cooker, Steamer, Sauté, Yogurt Maker, Warmer & Sterilizer, Includes App With Over 800 Recipes, Stainless Steel, 3 Quart is now {self.price} \n {self.item_url}",
                 from_="+18145645908",
                 to="+2348025959940",
             )
