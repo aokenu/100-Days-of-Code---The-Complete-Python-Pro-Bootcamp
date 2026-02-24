9a8f3018775e657e798b606062b28c30bc28c973
aokenu <austineokenu@gmail.com>
2026-02-24 10:15:00 +0100

    update project file

--- Day 49/main.py
+++ Day 49/main.py
@@ -3,3 +3,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+
+
+URL = "https://appbrewery.github.io/gym/"
