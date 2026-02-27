c894124517abc9329fb1db9e4b2e31d4bc4675a7
aokenu <austineokenu@gmail.com>
2026-02-27 10:15:00 +0100

    update project file

--- Day 49/main.py
+++ Day 49/main.py
@@ -3,14 +3,24 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import os
 
+ACCOUNT_EMAIL = "datadotng@gmail.com" 
+ACCOUNT_PASSWORD = "NP7tZsYipA5XRGC"
+GYM_URL = "https://appbrewery.github.io/gym/"
 
-URL = "https://appbrewery.github.io/gym/"
-
-chrome_options = webdriver.ChromeDrivers()
+# Keep Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
+# creating an instance of the webdriver
+driver = webdriver.Chrome(options=chrome_options)
 
-# creating an instance of the driver
-driver = webdriver.Chrome(option=chrome_options)
-driver.get(URL)
+
+# create a user profile for chrome
+user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
+
+chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+
+# launch the browser
+driver.get(GYM_URL)
